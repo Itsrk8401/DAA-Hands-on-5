9b2cfa8ca9ef14c8789f31200012658fe26f0510
Itsrk8401 <158119038+Itsrk8401@users.noreply.github.com>
2024-02-19 11:36:45 -0600

    Create heap.java

---
+++ heap.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+public class MinHeap<T extends Comparable<T>> {
+    private ArrayList<T> heap;
+
+    public MinHeap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public void buildMinHeap(ArrayList<T> data) {
+        this.heap = new ArrayList<>(data);
+        int n = data.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(i);
+        }
+    }
+
+    private void heapify(int i) {
+        int smallest = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < heap.size() && heap.get(leftChild).compareTo(heap.get(smallest)) < 0) {
+            smallest = leftChild;
+        }
+
+        if (rightChild < heap.size() && heap.get(rightChild).compareTo(heap.get(smallest)) < 0) {
+            smallest = rightChild;
+        }
+
+        if (smallest != i) {
+            swap(i, smallest);
+            heapify(smallest);
+        }
+    }
+
+    public void push(T value) {
+        heap.add(value);
+        int i = heap.size() - 1;
+        while (i > 0 && heap.get((i - 1) / 2).compareTo(heap.get(i)) > 0) {
+            swap(i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    public T pop() {
+        if (heap.isEmpty()) {
+            return null;
+        }
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        T root = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        heapify(0);
+        return root;
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> data = new ArrayList<>();
+        data.add(4);
+        data.add(10);
+        data.add(3);
+        data.add(5);
+        data.add(1);
+
+        MinHeap<Integer> minHeap = new MinHeap<>();
+        minHeap.buildMinHeap(data);
+
+        System.out.println("Heap: " + minHeap.heap);  // [1, 4, 3, 10, 5]
+        System.out.println("Pop: " + minHeap.pop());  // 1
+        System.out.println("Heap after pop: " + minHeap.heap);  // [3, 4, 5, 10]
+        minHeap.push(2);
+        System.out.println("Heap after push(2): " + minHeap.heap);  // [2, 4, 3, 10, 5]
+    }
+}
